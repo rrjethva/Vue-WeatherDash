8aca076eb6fbc17e0939c59a854ce00b595567c6
Ravi Jethva <rrjethva@gmail.com>
2020-06-23 17:36:18 -0400

    removed production mode, caused build failure in heroku

--- webpack.config.js
+++ webpack.config.js
@@ -58,10 +58,6 @@ module.exports = {
  devtool: '#eval-source-map'
 }
 
-module.exports = {
-  mode: 'production'
-}
-
 if (process.env.NODE_ENV === 'production') {
  module.exports.devtool = '#source-map'
  // http://vue-loader.vuejs.org/en/workflow/production.html
